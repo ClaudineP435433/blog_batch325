4f8fa57bcaefc4c1f4149b5f548acc339ace2932
Claudine <clo.pradal@gmail.com>
2019-11-15 10:41:35 +0100

    gem cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_081159) do
+ActiveRecord::Schema.define(version: 2019_11_15_092823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_081159) do
     t.integer "like"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
 end
